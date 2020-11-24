f875bd4e495556d8ce279e067a897f07d9314bd8
eshux <54838488+eshux@users.noreply.github.com>
2020-11-24 14:50:20 +0200

    homepage

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,9 @@ const App = () => {
           <Route exact path="/">
             <Home />
           </Route>
+          <Route exact path="/blog/">
+            <Home />
+          </Route>
           <Route exact path="/about">
             <About />
           </Route>
